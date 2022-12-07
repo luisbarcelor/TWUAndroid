a23f9eba758724595d81b9d6719dec184b09014a
luisbarcelor <luisbarcelor@hotmail.com>
2022-12-07 02:30:03 +0100

    Home view fix

--- app/src/main/java/com/twu_android/menus/HomeFragment.java
+++ app/src/main/java/com/twu_android/menus/HomeFragment.java
@@ -3,7 +3,6 @@ package com.twu_android.menus;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,12 +10,10 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.SearchView;
 
 import com.twu_android.R;
-import com.twu_android.menus.home_categories.DesktopFragment;
+import com.twu_android.menus.home_categories.GeneralFragment;
 
 public class HomeFragment extends Fragment {
     @SuppressLint("ClickableViewAccessibility")
@@ -26,13 +23,28 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        DesktopFragment desktopFragment = new DesktopFragment();
+        GeneralFragment generalFragment = new GeneralFragment();
         FragmentManager fManager = getChildFragmentManager();
         fManager.beginTransaction()
-                .replace(R.id.home_content, desktopFragment, "Desktop")
+                .replace(R.id.home_content, generalFragment, "General")
                 .commit();
 
         LinearLayout upper_menu = view.findViewById(R.id.upper_menu);
+        for (int i = 0; i < upper_menu.getChildCount(); i++) {
+            View upper_menu_child = upper_menu.getChildAt(i);
+            upper_menu_child.setTag(i);
+            upper_menu_child.setOnClickListener(v -> {
+                switch ((int) v.getTag()) {
+                    case 0 -> System.out.println("Desktop");
+                    case 1 -> System.out.println("Laptop");
+                    case 2 -> System.out.println("Network");
+                    case 3 -> System.out.println("Phones");
+                    case 4 -> System.out.println("Peripherals");
+                    case 5 -> System.out.println("Headphones");
+                    case 6 -> System.out.println("Services");
+                }
+            });
+        }
 
         return view;
     }
--- app/src/main/java/com/twu_android/menus/home_categories/LaptopFragment.java
+++ app/src/main/java/com/twu_android/menus/home_categories/LaptopFragment.java
@@ -12,10 +12,10 @@ import com.twu_android.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DesktopFragment#newInstance} factory method to
+ * Use the {@link LaptopFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DesktopFragment extends Fragment {
+public class LaptopFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class DesktopFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DesktopFragment() {
+    public LaptopFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class DesktopFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DesktopFragment.
+     * @return A new instance of fragment LaptopFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DesktopFragment newInstance(String param1, String param2) {
-        DesktopFragment fragment = new DesktopFragment();
+    public static LaptopFragment newInstance(String param1, String param2) {
+        LaptopFragment fragment = new LaptopFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class DesktopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_desktop, container, false);
+        return inflater.inflate(R.layout.fragment_laptop, container, false);
     }
 }
